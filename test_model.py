1870751b93e58e343f79cabbb72fa8247c60ed18
Vishnu Sivan <codemaker2015@gmail.com>
2021-03-14 12:20:51 +0530

    file based data input and output

--- test_model.py
+++ test_model.py
@@ -3,6 +3,7 @@ import pickle
 import nltk
 from nltk.corpus import stopwords
 import os
+from datetime import datetime
 
 import silence_tensorflow.auto
 from tensorflow.python.keras.models import Model, Sequential, model_from_json, load_model
@@ -27,29 +28,34 @@ with open('model/tokenizer.pickle', 'rb') as handle:
 
 try:
     while True:
-        input_ = input("Enter the paragraph:")
-#        data = ""
-#        with open(input_, 'r', encoding='utf8') as file:
-#            data = file.read()
-#        print(type(data))
-        new_t = Tokenizer()
-        new_t.fit_on_texts([input_])
-        tokens = [i for i in new_t.word_index.keys()]
-#		print(tokens)
-        actual_tokens = new_t.texts_to_sequences([input_])
-        inv_map_tokens = {v: k for k, v in new_t.word_index.items()}
-        actual_tokens = [inv_map_tokens[i] for i in actual_tokens[0]]
-        tokens = actual_tokens
-        input_ = tokenizer.texts_to_sequences([input_])
-        input_ = pad_sequences(input_, padding = "post", truncating = "post", maxlen = 25, value = 0)
-        output = model.predict([input_])
-        output = np.argmax(output, axis = -1)
-        where_ = np.where(output[0] == 1)[0]
-        output_keywords = np.take(tokens, where_)
-        output_keywords = [i for i in output_keywords if i not in stop_words]
-        output_keywords = list(set(output_keywords))
-#		print(tokens)
-#		print(output)
-        print(output_keywords)
+        final_results = []
+        input_ = input("Enter the file name:")
+        final_results.append('\nTimestamp: ' + str(datetime.now()) + '\nFile name: ' + input_ + '\nKeywords: ')
+        with open(input_) as fil:
+	        lines = fil.readlines()
+	        for line in lines:
+                    input_ = line
+                    new_t = Tokenizer()
+                    new_t.fit_on_texts([input_])
+                    tokens = [i for i in new_t.word_index.keys()]
+                    actual_tokens = new_t.texts_to_sequences([input_])
+                    inv_map_tokens = {v: k for k, v in new_t.word_index.items()}
+                    actual_tokens = [inv_map_tokens[i] for i in actual_tokens[0]]
+                    tokens = actual_tokens
+                    input_ = tokenizer.texts_to_sequences([input_])
+                    input_ = pad_sequences(input_, padding = "post", truncating = "post", maxlen = 25, value = 0)
+                    output = model.predict([input_])
+                    output = np.argmax(output, axis = -1)
+                    where_ = np.where(output[0] == 1)[0]
+                    output_keywords = np.take(tokens, where_)
+                    output_keywords = [i for i in output_keywords if i not in stop_words]
+                    output_keywords = list(set(output_keywords))
+                    final_results.append(', '.join(output_keywords))
+        
+        print("Keywords: " + str(final_results))
+        # Storing the output
+        with open('final_results.csv', 'a') as fil:
+            fil.writelines(final_results)
+
 except KeyboardInterrupt:
     pass
